f32f969ac21f31138df6c0c9cfbc0af65e982bed
Vikram Boya <vikboya3044@TMXSYS.local>
2021-02-10 16:08:35 +0530

    ädded the mission_30 initial project files

---
+++ src/main/java/com/mission_30/Mission30Application.java
@@ -0,0 +1,13 @@
+package com.mission_30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mission30Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mission30Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mission_30/Mission30ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mission_30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mission30ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
